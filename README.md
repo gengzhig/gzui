a61435137b69e58fba2d73b1e914e8bcda0d6f5e
gengzhig <2016995256@qq.com>
2021-09-10 15:20:25 +0800

    描述文档修改

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: gz
  * @Date: 2021-08-11 09:36:48
  * @LastEditors: gz
- * @LastEditTime: 2021-09-09 10:55:03
+ * @LastEditTime: 2021-09-10 15:20:01
  * @Description: file content
  * @FilePath: \gi-ui\README.md
 -->
@@ -12,7 +12,9 @@
 
 1. tabs标签页
 1. table表格
+1. 分页器
 1. 虚拟列表
+1. 输入框
 1. 下拉菜单
 1. 五星好评
 1. 模态框
--- src/layouts/components/NavBar.vue
+++ src/layouts/components/NavBar.vue
@@ -1,44 +1,45 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-30 10:49:17
- * @LastEditTime: 2021-01-30 11:50:47
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-09-10 15:18:19
+ * @LastEditors: gz
  * @Description: In User Settings Edit
- * @FilePath: \zimeigui-vite2-vue3\src\layouts\components\Navbar.vue
+ * @FilePath: \gi-ui\src\layouts\components\NavBar.vue
 -->
 <template>
-  <div class="nav-bar">
-    <h3 class="title">gzUI组件库</h3>
-    <el-avatar icon="el-icon-user-solid"></el-avatar>
-  </div>
+	<div class="nav-bar">
+		<h3 class="title" @click="goHome">gzUI组件库</h3>
+		<el-avatar icon="el-icon-user-solid"></el-avatar>
+	</div>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {};
-  },
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+const goHome = (e) => {
+	router.push("/home");
 };
 </script>
 
 <style lang="scss" scoped>
 .nav-bar {
-  width: 100vw;
-  height: 60px;
-  line-height: 60px;
-  background: #333;
-  color: #fff;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 2;
-  box-sizing: border-box;
-  padding: 0 30px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  .title {
-    letter-spacing: 0.2em;
-  }
+	width: 100vw;
+	height: 60px;
+	line-height: 60px;
+	background: #333;
+	color: #fff;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 2;
+	box-sizing: border-box;
+	padding: 0 30px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	.title {
+		letter-spacing: 0.2em;
+    cursor: pointer;
+	}
 }
 </style>
