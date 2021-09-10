10344e3e68ffdb5f24d3d2eb456e66a35f15b671
gengzhig <2016995256@qq.com>
2021-09-10 15:14:08 +0800

    描述修改

--- src/views/comp/custom/input.vue
+++ src/views/comp/custom/input.vue
@@ -2,7 +2,7 @@
  * @Author: gz
  * @Date: 2021-09-10 13:44:26
  * @LastEditors: gz
- * @LastEditTime: 2021-09-10 15:12:38
+ * @LastEditTime: 2021-09-10 15:13:42
  * @Description: file content
  * @FilePath: \gi-ui\src\views\comp\custom\input.vue
 -->
@@ -10,8 +10,7 @@
 	<div class="page-component">
 		<h1>Input 输入框</h1>
 		<p>
-			Input 为受控组件，它总会显示 Vue 绑定值。 通常情况下，应当处理 input
-			事件，并更新组件的绑定值（或使用v-model）。否则，输入框内显示的值将不会改变。 不支持 v-model 修饰符。
+			支持普通输入，密码可禁选，文本域。
 		</p>
 		<h3>基础用法</h3>
 
