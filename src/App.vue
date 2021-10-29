b3e4af08245e32d0dd7636dd832d6fe39a723e1d
gengzhig <2016995256@qq.com>
2021-10-29 18:00:02 +0800

    1231231

--- src/App.vue
+++ src/App.vue
@@ -17,3 +17,40 @@ import { provide } from "vue";
 provide("compInfo", ui.compInfo);
 provide("$tool", tool);
 </script>
+<style>
+.title {
+	display: block;
+	text-decoration: none;
+	text-align: center;
+	line-height: 1.5;
+	margin: 20px 0px;
+	background-image: -webkit-linear-gradient(
+		left,
+		#3498db,
+		#f47920 10%,
+		#d71345 20%,
+		#f7acbc 30%,
+		#ffd400 40%,
+		#3498db 50%,
+		#f47920 60%,
+		#d71345 70%,
+		#f7acbc 80%,
+		#ffd400 90%,
+		#3498db
+	);
+	color: transparent;
+	-webkit-background-clip: text;
+	background-size: 200% 100%;
+	animation: slide 5s infinite linear;
+	font-size: 40px;
+}
+@keyframes slide {
+	0% {
+		background-position: 0 0;
+	}
+
+	100% {
+		background-position: -100% 0;
+	}
+}
+</style>
