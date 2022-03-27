550b1e74fa5bf1fe4f0a12adbc405343cc89d0f5
gengzhig <2016995256@qq.com>
2022-03-27 10:46:18 +0800

    docs

--- 2.js
+++
@@ -1,7 +0,0 @@
-console.time("worker2时间")
-let a = 0;
-for (let i = 0; i < 160000000; i++) {
-  a += i;
-}
-console.timeEnd("worker2时间")
-postMessage(a);
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -25,25 +25,40 @@ module.exports = {
 		//   侧边导航
 		sidebar: [
 			{
+				text: "布局",
+				children: [
+					{ text: "栅格", link: "/components/button/" },
+				],
+			},
+			{
 				text: "通用",
 				children: [
 					{ text: "按钮", link: "/components/button/" },
+					{ text: "分割线", link: "/components/button/" },
 					{ text: "轮播图", link: "/components/carousel/" },
 					{ text: "文本省略", link: "/components/ellipsis/" },
 					{ text: "图片", link: "/components/image/" },
+					{ text: "渐变文字", link: "/components/image/" },
 				],
 			},
 			{
 				text: "数据录入",
 				children: [
 					{ text: "输入框", link: "/components/input/" },
+					{ text: "数字输入", link: "/components/input/" },
+					{ text: "单选框", link: "/components/input/" },
+					{ text: "复选框", link: "/components/input/" },
 					{ text: "选择器", link: "/components/selector/" },
+					{ text: "日期选择器", link: "/components/selector/" },
 					{ text: "五星好评", link: "/components/fiveStar/" },
+					{ text: "开关", link: "/components/fiveStar/" },
+					{ text: "表单", link: "/components/fiveStar/" },
 				],
 			},
 			{
 				text: "数据展示",
 				children: [
+					{ text: "日历", link: "/components/virtualList/" },
 					{ text: "虚拟列表", link: "/components/virtualList/" },
 					{ text: "Tableb表格", link: "/components/table/" },
 					{ text: "树组件", link: "/components/tree/" },
@@ -55,11 +70,18 @@ module.exports = {
 				children: [
 					{ text: "分页器", link: "/components/pagination/" },
 					{ text: "Tabs标签页", link: "/components/tabs/" },
+					{ text: "多级菜单", link: "/components/pagination/" },
+					{ text: "面包屑", link: "/components/tabs/" },
 				],
 			},
 			{
 				text: "反馈",
-				children: [{ text: "模态框", link: "/components/modalBox/" }],
+				children: [
+					{ text: "模态框", link: "/components/modalBox/" },
+					{ text: "抽屉", link: "/components/modalBox/" },
+					{ text: "加载", link: "/components/modalBox/" },
+					{ text: "Message消息", link: "/components/modalBox/" }
+				],
 			},
 		],
 	},
